346664eaaf203ccd7313c05e748c87e07e08ce32
James Garner <james@jadaradix.com>
2014-09-26 11:58:12 +0100

    Fix internal links to blog content via blog post ID. Clean up blogController.

---
+++ js/angular-instance.js
@@ -0,0 +1,71 @@
+var jadaSite = angular.module('jadaSite', []).config(function($sceProvider) {
+  $sceProvider.enabled(false);
+});
+
+
+var ajaxRequestCount = 0;
+var ajaxRequestTotal = 1;
+function easyAjax(url, callback) {
+
+  function ajaxDone() {
+    ajaxRequestCount += 1;
+    if (ajaxRequestCount == ajaxRequestTotal) {
+      fadeAll();
+      ajaxRequestCount = 0;
+    }
+  }
+
+  function ajaxSuccess(data) {
+    callback(data);
+    ajaxDone();
+  }
+
+  function ajaxFail(error) {
+    console.log(error);
+    callback(null);
+    ajaxDone();
+  }
+
+  var ajaxRequest = $.ajax(url);
+  ajaxRequest.done(function(response) {
+    if (!response['error']) {
+      ajaxSuccess(response);
+    } else {
+      ajaxFail(response['error']);
+    }
+  });
+  ajaxRequest.fail(function(response) {
+    ajaxFail("I couldn't reach '" + url + "'");
+  });
+
+}
+
+
+jadaSite.controller('blogController', function ($scope) {
+
+  $scope.tileRows = [];
+
+  $scope.hasTileRows = function() {
+    return ($scope.tileRows.length > 0); 
+  };
+
+  easyAjax("api/blog", function(data) {
+    if (!data) return;
+    var r = [];
+    $.each(data, function(index, blogPost) {
+      if ((index + 1) % 2) r.push([]);
+      r[r.length - 1].push(blogPost);
+    });
+    $scope.$apply(function() {
+      $scope.tileRows = r;
+    });
+  });
+
+});
+
+
+jadaSite.controller('gitController', function ($scope) {
+
+  $scope.gitData = {};
+
+});
